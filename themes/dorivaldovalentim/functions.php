add8d27880a871b67e708b588be67c0d95f3cfe9
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2023-05-07 01:38:54 +0100

    Adicionei o functions.php

---
+++ themes/dorivaldovalentim/functions.php
@@ -0,0 +1,17 @@
+<?php
+if (!function_exists('dorivaldovalentim_setup')):
+/**
+ * Sets up theme defaults and registers support for various WordPress
+ * features.
+ *
+ * It is important to set up these functions before the init hook so
+ * that none of these features are lost.
+ *
+ *  @since Dorivaldo Valentim 1.0
+ */
+    function dorivaldovalentim_setup()
+    {
+        
+    }
+
+endif;
