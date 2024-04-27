37dae072f1a6531ce878402e473afb598e345a44
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2024-04-27 14:36:03 +0100

    Fiz as primeiras configurações iniciais no template do svelte

--- resources/js/Pages/Welcome.svelte
+++ resources/js/Pages/Welcome.svelte
@@ -1,5 +1,7 @@
-<script></script>
+<script>
+    import Layout from "@/Layouts/Auth.svelte";
+</script>
 
-<section>
-    Hello World!
-</section>
+<Layout title="Página Inicial">
+    <section>Hello World!</section>
+</Layout>
