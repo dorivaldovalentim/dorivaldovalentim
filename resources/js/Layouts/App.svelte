3a0f038501173b10e4fcac827c872227a28af787
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2024-05-09 17:12:02 +0100

    Preparei o escopo para o front

--- resources/js/Layouts/App.svelte
+++ resources/js/Layouts/App.svelte
@@ -1,5 +1,6 @@
 <script>
     import Menu from "@/Components/Menu.svelte";
+    import Aside from "@/Components/Aside.svelte";
 
     export let title;
 </script>
@@ -9,6 +10,17 @@
 </svelte:head>
 
 <main>
-    <Menu></Menu>
-    <slot></slot>
+    <div class="row">
+        <div class="col-12 col-md-3">
+            <Aside></Aside>
+        </div>
+
+        <div class="col-12 col-md-8">
+            <slot></slot>
+        </div>
+
+        <div class="col-12 col-md-1">
+            <Menu></Menu>
+        </div>
+    </div>
 </main>
