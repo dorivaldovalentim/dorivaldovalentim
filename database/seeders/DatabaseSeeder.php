b4efc8ff6f3e70f9e058f8875993aadbb2fe4b49
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2024-05-11 19:18:31 +0100

    Adicionei um seeder com um cadastro de dados gerais

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,11 @@
 
 namespace Database\Seeders;
 
+use Hash;
+use App\Models\Client;
+use App\Models\Skill;
+use App\Models\SocialNetwork;
+use App\Models\Technology;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +20,30 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        User::create([
+            "name" => env("DEFAULT_USER"),
+            "email" => env("DEFAULT_EMAIL"),
+            "password" => Hash::make(env("DEFAULT_PASSWORD")),
         ]);
+
+        // Seed skills
+        Skill::create(['skill' => 'BackEnd']);
+        Skill::create(['skill' => 'FrontEnd']);
+
+        // Seed clients
+        Client::create(['name' => 'API Soluções', 'site' => 'https://apisolucoes.com']);
+        Client::create(['name' => 'Digital Factory', 'site' => 'https://digitalfactory.com']);
+        Client::create(['name' => 'Zap', 'site' => 'https://zap.com']);
+
+        // Generate and seed additional technologies
+        $additionalTechnologies = ['HTML', 'CSS', 'JavaScript', 'PHP', 'Python', 'Ruby', 'Java', 'React', 'Vue.js', 'Angular', 'Node.js', 'SQL'];
+        foreach ($additionalTechnologies as $tech) {
+            Technology::create(['technology' => $tech]);
+        }
+
+        // Seed social networks
+        SocialNetwork::create(['name' => 'Facebook', 'link' => 'https://facebook.com', 'icon' => '<svg>...</svg>']);
+        SocialNetwork::create(['name' => 'Twitter', 'link' => 'https://twitter.com', 'icon' => '<svg>...</svg>']);
+        SocialNetwork::create(['name' => 'Instagram', 'link' => 'https://instagram.com', 'icon' => '<svg>...</svg>']);
     }
 }
