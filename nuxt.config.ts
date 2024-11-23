79b7967441f4c52e532a6fe64ba735cbc514c348
Dorivaldo Valentim <54032975+dorivaldovalentim@users.noreply.github.com>
2024-11-23 05:24:36 +0100

    feat: configuração do @nuxtjs/i18n

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/strapi'],
+  modules: ['@nuxtjs/strapi', '@nuxtjs/i18n'],
 
   runtimeConfig: {
     app: {
@@ -10,5 +10,19 @@ export default defineNuxtConfig({
         token: process.env.STRAPI_API_TOKEN
       }
     }
-  }
+  },
+
+  i18n: {
+    defaultLocale: 'pt',
+    langDir: "lang",
+    strategy: 'prefix_except_default',
+    customRoutes: 'page',
+    locales: [
+      { code: 'pt', name: 'Português', file: 'pt.ts' },
+      { code: 'en', name: 'Inglês', file: 'en.ts' }
+    ],
+    compilation: {
+      strictMessage: false
+    }
+  },
 })
