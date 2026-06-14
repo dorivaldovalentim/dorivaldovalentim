33b4b0e6b51a3ef633e0d691c653859bf10beaaa
Dorivaldo Valentim <dorivaldovalentim@gmail.com>
2026-06-14 14:22:55 +0100

    feat: setting up google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: ['nuxt-gtag'],
+  runtimeConfig: {
+    public: {
+      gtagId: process.env.NUXT_PUBLIC_GTAG_ID ?? '',
+    }
+  },
+  gtag: {
+    id: process.env.NUXT_PUBLIC_GTAG_ID ?? ''
+  }
 })
